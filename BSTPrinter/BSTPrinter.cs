fe75c2830eff8eafac6269af335a38f956ec2f7d
karl mogensen <karlmogensen@hotmail.com>
2020-05-28 16:45:30 +0200

    better SOC

--- BSTPrinter/BSTPrinter.cs
+++ BSTPrinter/BSTPrinter.cs
@@ -21,7 +21,7 @@ namespace BSTPrinter
         /// 
         /// </summary>
         /// <param name="binaryTreePreOrder">Should be pre order tree values as single spaced integers</param>
-        /// <param name="distance">The number of slashes between each node </param>
+        /// <param name="distance">Sets the distance between each node </param>
         static public void Print(string binaryTreePreOrder, int distance)
         {
             Console.WriteLine(GetPrintableString(binaryTreePreOrder, distance));
@@ -31,75 +31,22 @@ namespace BSTPrinter
         /// Returns a printable string of BST structure
         /// </summary>
         /// <param name="binaryTreePreOrder">String should be pre order tree values as single spaced integers</param>
-        /// <param name="distance">The number of slashes between each node </param>
+        /// <param name="distance">Sets the distance between each node </param>
         /// <returns></returns>
         public static string GetPrintableString(string binaryTreePreOrder, int distance)
         {
 
             if (distance < 1)
                 throw new FormatException("Invalid distance, must be atleast 1");
-            else if (binaryTreePreOrder.Length == 0 || binaryTreePreOrder == " ")
+            if (binaryTreePreOrder.Length == 0 || binaryTreePreOrder == " ")
                 throw new FormatException("Invalid format of input string");
 
             var points = GetPoints(binaryTreePreOrder, distance);
 
-            //get dimensions of grid
-            int height = points.Max(p => p.Y) + 1;
-            int smallestX = points.Min(p => p.SmallestX);
-            int biggestX = (points.Max(p => p.BiggestX));
-            int width = ((smallestX * -1) + biggestX) + 50;
-
-
-            //init grid with spaces 
-            var grid = new string[height, width];
-            for (int i = 0; i < height; i++)
-                for (int j = 0; j < width; j++)
-                    grid[i, j] = " ";
-
-            //fill with data
-            foreach (BSTPrinterPoint p in points)
-            {
-                int realX = p.X + (smallestX * -1);
-                if (p.IsComposite)
-                {
-                    int offset = 0;
-                    bool didOffset = false;
-                    foreach (var subPoint in p.Points)
-                    {
-                        if (grid[subPoint.Y, subPoint.X + (smallestX * -1)] != " " && !didOffset)
-                        {
-                            didOffset = true;
-                            while (grid[subPoint.Y, subPoint.X + (smallestX * -1) + offset] != " ")
-                                offset++;
-                            grid[subPoint.Y, subPoint.X + (smallestX * -1) + offset] = "x";
-                            offset++;
-                        }
-                        grid[subPoint.Y, subPoint.X + (smallestX * -1) + offset] = subPoint.Data;
-                    }
-                }
-                else
-                {
-                    if (grid[p.Y, realX] != " ")
-                    {
-                        grid[p.Y, realX + 1] = "x";
-                        grid[p.Y, realX + 2] = p.Data;
-                    }
-                    else
-                        grid[p.Y, realX] = p.Data;
-                }
-            }
-
-
-            string printable = "";
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                    printable += grid[i, j];
-                printable += "\n";
-            }
-
-            return printable;
+            var grid = new Grid(points);
+            return grid.GetPrintable();
         }
+
         /// <summary>
         /// >Returns a printable string of BST structure
         /// </summary>
@@ -109,6 +56,7 @@ namespace BSTPrinter
         {
             return GetPrintableString(binaryTreePreOrder, 1);
         }
+
 
 
 
@@ -133,7 +81,8 @@ namespace BSTPrinter
 
             return tree.GetPoints();
         }
-        class BSTPrinterTree
+
+        private class BSTPrinterTree
         {
             int slashes;
             public BSTPrinterTree(int slashes)
@@ -164,7 +113,7 @@ namespace BSTPrinter
                 return points;
             }
         }
-        class BSTPrinterNode
+        private class BSTPrinterNode
         {
             public BSTPrinterNode LeftChild;
             public BSTPrinterNode RightChild;
@@ -230,7 +179,7 @@ namespace BSTPrinter
                 }
             }
         }
-        public class BSTPrinterPoint
+        private class BSTPrinterPoint
         {
             public int X;
             public int Y;
@@ -278,6 +227,86 @@ namespace BSTPrinter
                     X = x;
                     Data = data;
                 }
+            }
+        }
+
+        private class Grid
+        {
+            List<BSTPrinterPoint> points;
+            int smallestX;
+            int biggestX;
+
+            public Grid(List<BSTPrinterPoint> points)
+            {
+                this.points = points;
+                smallestX = points.Min(p => p.SmallestX);
+                biggestX = points.Max(p => p.BiggestX);
+            }
+
+            int xSpacer = 50;
+            public string GetPrintable()
+            {
+                int height = points.Max(p => p.Y) + 1;
+                int width = ((smallestX * -1) + biggestX) + xSpacer;
+                var grid = GetEmptyGrid(height, width);
+
+                foreach (BSTPrinterPoint p in points)
+                {
+                    int realX = p.X + (smallestX * -1);
+                    if (p.IsComposite)
+                    {
+                        int offset = 0;
+                        bool didOffset = false;
+                        foreach (var subPoint in p.Points)
+                        {
+                            if (grid[subPoint.Y, subPoint.X + (smallestX * -1)] != " " && !didOffset)
+                            {
+                                didOffset = true;
+                                while (grid[subPoint.Y, subPoint.X + (smallestX * -1) + offset] != " ")
+                                    offset++;
+                                grid[subPoint.Y, subPoint.X + (smallestX * -1) + offset] = "x";
+                                offset++;
+                            }
+                            grid[subPoint.Y, subPoint.X + (smallestX * -1) + offset] = subPoint.Data;
+                        }
+                    }
+                    else
+                    {
+                        if (grid[p.Y, realX] != " ")
+                        {
+                            grid[p.Y, realX + 1] = "x";
+                            grid[p.Y, realX + 2] = p.Data;
+                        }
+                        else
+                            grid[p.Y, realX] = p.Data;
+                    }
+                }
+
+                return ArrayToString(grid);
+            }
+
+            string ArrayToString(string[,] grid)
+            {
+                string printable = "";
+                for (int i = 0; i < grid.GetLength(0); i++)
+                {
+                    for (int j = 0; j < grid.GetLength(1); j++)
+                        printable += grid[i, j];
+                    printable += "\n";
+                }
+
+                return printable;
+            }
+
+            string[,] GetEmptyGrid(int height, int width)
+            {
+                var grid = new string[height, width];
+                for (int i = 0; i < height; i++)
+                    for (int j = 0; j < width; j++)
+                        grid[i, j] = " ";
+
+
+                return grid;
             }
         }
     }
--- BSTPrinter/Program.cs
+++ BSTPrinter/Program.cs
@@ -10,26 +10,19 @@ namespace BSTPrinter
             var tree = new BinarySearchTree();
 
             tree.Insert(100);
-            tree.Insert(120);
-            tree.Insert(102);
-            tree.Insert(105);
-            tree.Insert(101);
-            tree.Insert(90);
-            tree.Insert(80);
             tree.Insert(10);
-            tree.Insert(11);
-            tree.Insert(9);
+            tree.Insert(120);
             tree.Insert(2000);
             tree.Insert(2225);
             tree.Insert(2233);
-            tree.Insert(2122);
 
             var preOrder = tree.GetPreOrderString();
             Console.WriteLine(preOrder);
 
-            BSTPrinter.Print(preOrder,0);
+            BSTPrinter.Print(preOrder);
 
 
         }
+
     }
 }
