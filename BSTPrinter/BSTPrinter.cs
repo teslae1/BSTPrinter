2c30e471d4cc41223377dc931aa1030fe7d28c5e
karl mogensen <karlmogensen@hotmail.com>
2020-06-11 07:57:06 +0200

    Refacoring tree

--- BSTPrinter/BSTPrinter.cs
+++ BSTPrinter/BSTPrinter.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using BSTPrinter.BST;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
-
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace BSTPrinter
 {
@@ -16,35 +21,16 @@ namespace BSTPrinter
         {
             Console.WriteLine(GetPrintableString(binaryTreePreOrder));
         }
+
         /// <summary>
         /// Prints BST 
         /// 
         /// </summary>
         /// <param name="binaryTreePreOrder">Should be pre order tree values as single spaced integers</param>
-        /// <param name="distance">Sets the distance between each node </param>
-        static public void Print(string binaryTreePreOrder, int distance)
+        /// <param name="distanceBetweenNodes">Sets the distance between each node </param>
+        static public void Print(string binaryTreePreOrder, int distanceBetweenNodes)
         {
-            Console.WriteLine(GetPrintableString(binaryTreePreOrder, distance));
-        }
-
-        /// <summary>
-        /// Returns a printable string of BST structure
-        /// </summary>
-        /// <param name="binaryTreePreOrder">String should be pre order tree values as single spaced integers</param>
-        /// <param name="distance">Sets the distance between each node </param>
-        /// <returns></returns>
-        public static string GetPrintableString(string binaryTreePreOrder, int distance)
-        {
-
-            if (distance < 1)
-                throw new FormatException("Invalid distance, must be atleast 1");
-            if (binaryTreePreOrder.Length == 0 || binaryTreePreOrder == " ")
-                throw new FormatException("Invalid format of input string");
-
-            var points = GetPoints(binaryTreePreOrder, distance);
-
-            var grid = new Grid(points);
-            return grid.GetPrintable();
+            Console.WriteLine(GetPrintableString(binaryTreePreOrder, distanceBetweenNodes));
         }
 
         /// <summary>
@@ -57,126 +43,213 @@ namespace BSTPrinter
             return GetPrintableString(binaryTreePreOrder, 1);
         }
 
-
-
-
-        static void RemoveNotNumbers(ref string[] arr)
+        /// <summary>
+        /// Returns a printable string of BST structure
+        /// </summary>
+        /// <param name="binaryTreePreOrder">String should be pre order tree values as single spaced integers</param>
+        /// <param name="distanceBetweenNodes">Sets the distance between each node </param>
+        /// <returns></returns>
+        public static string GetPrintableString(string binaryTreePreOrder, int distanceBetweenNodes)
         {
-            var filtered = new List<string>();
-            int n;
-            foreach (var s in arr)
-                if (int.TryParse(s, out n))
-                    filtered.Add(s);
+            SetDistanceBetweenNodes(distanceBetweenNodes);
+            if (InputValidated(binaryTreePreOrder))
+            {
+                var points = GetPoints(binaryTreePreOrder);
+                var grid = new Grid(points);
+                return grid.GetPrintable();
+            }
 
-            arr = filtered.ToArray();
+            return null;
         }
-        static List<BSTPrinterPoint> GetPoints(string binaryTreePreOrder, int slashes)
+
+        private static int distanceBetweenNodes;
+        private static void SetDistanceBetweenNodes(int distance)
+        {
+            distanceBetweenNodes = distance;
+        }
+
+        static bool InputValidated(string binaryTreePreOrder)
+        {
+            if (distanceBetweenNodes < 1)
+                throw new FormatException("Invalid distance, must be atleast 1");
+            if (binaryTreePreOrder.Length == 0 ||
+                binaryTreePreOrder == " " ||
+                ToNumbers(binaryTreePreOrder.Split(' ')).Count < 1)
+                throw new FormatException("Invalid format of pre-order values");
+
+            return true;
+        }
+
+        static List<BSTPrinterPoint> GetPoints(string binaryTreePreOrder)
         {
             string[] arr = binaryTreePreOrder.Split(' ');
-            RemoveNotNumbers(ref arr);
+            var nodeValues = ToNumbers(arr);
+            var tree = new BSTPrinterTree();
 
-            var tree = new BSTPrinterTree(slashes);
-            foreach (var s in arr)
-                tree.Insert(s);
-
-            return tree.GetPoints();
+            return tree.GetPoints(nodeValues);
         }
+
+        static List<int> ToNumbers(string[] arr)
+        {
+            var numbers = new List<int>();
+            int numb;
+            foreach (string s in arr)
+                if (int.TryParse(s, out numb))
+                    numbers.Add(numb);
+
+            return numbers;
+        }
+
 
         private class BSTPrinterTree
         {
-            int slashes;
-            public BSTPrinterTree(int slashes)
-            {
-                this.slashes = slashes;
-            }
-            BSTPrinterNode root;
-            public void Insert(string value)
-            {
-                var nodeToInsert = new BSTPrinterNode(Convert.ToInt32(value));
-                if (root == null)
-                {
-                    root = nodeToInsert;
-                    var point = new BSTPrinterPoint(0, 0, value);
-                    nodeToInsert.Point = point;
-                    points = new List<BSTPrinterPoint>() { point };
-                }
-                else
-                    root.Insert(nodeToInsert,
-                        nodeToInsert.Data < root.Data ? root.Point.SmallestX : root.Point.BiggestX,
-                        0, ref points, slashes);
-            }
 
-            List<BSTPrinterPoint> points;
-            public List<BSTPrinterPoint> GetPoints()
+            static List<BSTPrinterPoint> points = new List<BSTPrinterPoint>();
+            Node root;
+
+
+            public List<BSTPrinterPoint> GetPoints(List<int> nodeValues)
             {
+                points.Clear();
+                foreach (int value in nodeValues)
+                    Insert(value);
+
                 root = null;
                 return points;
             }
-        }
-        private class BSTPrinterNode
-        {
-            public BSTPrinterNode LeftChild;
-            public BSTPrinterNode RightChild;
 
-            public BSTPrinterPoint Point;
-
-            public int Data;
-
-            public BSTPrinterNode(int data)
+            public void Insert(int value)
             {
-                this.Data = data;
+                var nodeToInsert = new Node(value);
+                if (root == null)
+                    SetupRoot(nodeToInsert);
+                else
+                {
+                    var parentCoordinate = new Coordinate(
+                        nodeToInsert.Data < root.Data ? root.Point.SmallestX : root.Point.BiggestX,
+                        0);
+                    root.Insert(nodeToInsert,parentCoordinate);
+                }
             }
 
-            public void Insert(BSTPrinterNode nodeToInsert, int x, int y, ref List<BSTPrinterPoint> points, int slashes)
+            void SetupRoot(Node root)
             {
+                this.root = root;
+                var point = new BSTPrinterPoint(new Coordinate(0, 0), root.Data.ToString());
+                points.Add(point);
+                root.Point = point;
+            }
 
-                if (nodeToInsert.Data < Data)
+            private class Node
+            {
+                public Node LeftChild;
+                public Node RightChild;
+
+                public BSTPrinterPoint Point;
+
+                public int Data;
+
+                public Node(int data)
                 {
-                    //go left
-                    if (LeftChild == null)
-                    {
-                        LeftChild = nodeToInsert;
-
-                        for (int i = 1; i <= slashes; i++)
-                            points.Add(new BSTPrinterPoint(x - i, y + i, "/"));
-
-                        var point = new BSTPrinterPoint((x - slashes) - nodeToInsert.Data.ToString().Length,
-                            y + (slashes + 1),
-                            nodeToInsert.Data.ToString());
-
-                        points.Add(point);
-                        LeftChild.Point = point;
-                    }
-                    else
-                        LeftChild.Insert(nodeToInsert,
-                            LeftChild.Data > nodeToInsert.Data ? LeftChild.Point.SmallestX : LeftChild.Point.BiggestX,
-                            y + (slashes + 1),
-                            ref points, slashes);
+                    this.Data = data;
                 }
-                else if (nodeToInsert.Data > Data)
+
+                public void Insert(Node nodeToInsert, Coordinate parentNodeCoordinate)
                 {
-                    //go right
-                    if (RightChild == null)
+
+                    if (nodeToInsert.Data < Data)
                     {
-                        RightChild = nodeToInsert;
-
-                        for (int i = 1; i <= slashes; i++)
-                            points.Add(new BSTPrinterPoint(x + i, y + i, "\\"));
-
-                        var point = new BSTPrinterPoint(
-                            x + (slashes + 1),
-                            y + (slashes + 1),
-                            nodeToInsert.Data.ToString());
-
-                        points.Add(point);
-                        RightChild.Point = point;
+                        //go left
+                        if (LeftChild == null)
+                            InsertAsLeftChild(nodeToInsert, parentNodeCoordinate);
+                        else
+                            TraverseLeft(nodeToInsert, parentNodeCoordinate);
                     }
-                    else
-                        RightChild.Insert(nodeToInsert,
-                            RightChild.Data > nodeToInsert.Data ? RightChild.Point.SmallestX : RightChild.Point.BiggestX,
-                            y + (slashes + 1),
-                            ref points, slashes);
+                    else if (nodeToInsert.Data > Data)
+                    {
+                        //go right
+                        if (RightChild == null)
+                            InsertAsRightChild(nodeToInsert, parentNodeCoordinate);
+                        else
+                            TraverseRight(nodeToInsert, parentNodeCoordinate);
+                    }
                 }
+
+                void TraverseLeft(Node nodeToInsert, Coordinate parentNodeCoordinate)
+                {
+                    var leftSideCoordinate = new Coordinate(
+                                LeftChild.Data > nodeToInsert.Data ? LeftChild.Point.SmallestX : LeftChild.Point.BiggestX,
+                                parentNodeCoordinate.Y + (distanceBetweenNodes + 1));
+                    LeftChild.Insert(nodeToInsert, leftSideCoordinate);
+                }
+                
+                void TraverseRight(Node nodeToInsert, Coordinate parentNodeCoordinate)
+                {
+                    var rightSideCoordinate = new Coordinate(
+                               RightChild.Data > nodeToInsert.Data ? RightChild.Point.SmallestX : RightChild.Point.BiggestX,
+                               parentNodeCoordinate.Y + (distanceBetweenNodes + 1));
+                    RightChild.Insert(nodeToInsert, rightSideCoordinate);
+                }
+
+                void InsertAsRightChild(Node node, Coordinate parentNodeCoordinate)
+                {
+                    RightChild = node;
+                    AddSlashesToRight(parentNodeCoordinate);
+                    AddNodePointRight(node, parentNodeCoordinate);
+                }
+                void AddSlashesToRight(Coordinate parentNodeCoordinate)
+                {
+                    for (int i = 1; i <= distanceBetweenNodes; i++)
+                    {
+                        var coordinate = new Coordinate(parentNodeCoordinate.X + i, parentNodeCoordinate.Y + i);
+                        points.Add(new BSTPrinterPoint(coordinate, "\\"));
+                    }
+                }
+                void AddNodePointRight(Node node, Coordinate parentNodeCoordinate)
+                {
+                    var coordinate = new Coordinate(
+                        parentNodeCoordinate.X + (distanceBetweenNodes + 1),
+                        parentNodeCoordinate.Y + (distanceBetweenNodes + 1));
+                    var point = new BSTPrinterPoint(coordinate, node.Data.ToString());
+                    points.Add(point);
+                    RightChild.Point = point;
+                }
+
+                void InsertAsLeftChild(Node node, Coordinate parentNodeCoordinate)
+                {
+                    LeftChild = node;
+                    AddSlashesToLeft(parentNodeCoordinate);
+                    AddNodePointLeft(node, parentNodeCoordinate);
+                }
+                void AddNodePointLeft(Node node, Coordinate parentNodeCoordinate)
+                {
+                    var nodePoint = new BSTPrinterPoint(
+                        new Coordinate((parentNodeCoordinate.X - distanceBetweenNodes) - node.Data.ToString().Length,
+                        parentNodeCoordinate.Y + (distanceBetweenNodes + 1)),
+                        node.Data.ToString());
+
+                    points.Add(nodePoint);
+                    LeftChild.Point = nodePoint;
+                }
+                void AddSlashesToLeft(Coordinate parentNodeCoordinate)
+                {
+                    for (int i = 1; i <= distanceBetweenNodes; i++)
+                        points.Add(new BSTPrinterPoint(
+                            new Coordinate(parentNodeCoordinate.X - i, parentNodeCoordinate.Y + i),
+                             "/"));
+                }
+            }
+
+        }
+
+        private class Coordinate
+        {
+            public int X;
+            public int Y;
+            public Coordinate(int x, int y)
+            {
+                X = x;
+                Y = y;
             }
         }
         private class BSTPrinterPoint
@@ -215,16 +288,18 @@ namespace BSTPrinter
             /// <param name="x"></param>
             /// <param name="y"></param>
             /// <param name="data"></param>
-            public BSTPrinterPoint(int x, int y, string data)
+            public BSTPrinterPoint(Coordinate coordinate, string data)
             {
 
-                Y = y;
+                Y = coordinate.Y;
                 if (data.Length > 1)
                     for (int i = 0; i < data.Length; i++)
-                        Points.Add(new BSTPrinterPoint(x + i, y, data[i].ToString()));
+                        Points.Add(new BSTPrinterPoint(
+                            new Coordinate(coordinate.X + i, coordinate.Y)
+                            , data[i].ToString()));
                 else
                 {
-                    X = x;
+                    X = coordinate.X;
                     Data = data;
                 }
             }
